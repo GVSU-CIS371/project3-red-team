c3fe399919624c06134860a0806e36eb1bd357b9
ZephyrsWhisper <hardyza@mail.gvsu.edu>
2024-03-27 21:37:38 -0400

    Major Update
    Recommit

--- src/App.vue
+++ src/App.vue
@@ -2,97 +2,21 @@
   <div>
     
     <Beverage :isIced="currentTemp === 'Cold'" :creamer="currCream" :beverage="currBase" :syrup="currSyrup"/>
-<<<<<<< Updated upstream
-    <ul>
-      <li>
-          <b>Temperature: </b>
-          <template v-for="temp in temps" :key="temp">
-=======
     
     <div id="selectorContainer">
 
       <AppDropdown buttonText="Temperature" class="contentButton">
         <div class="contentBox">
           <div v-for="temp in temps" :key="temp" class="itemBox">
->>>>>>> Stashed changes
             <label>
               <input
                 type="radio"
                 name="temperature"
-<<<<<<< Updated upstream
-                :id="`r${temp}`"
-=======
->>>>>>> Stashed changes
                 :value="temp"
                 v-model="currentTemp"
               />
               {{ temp }}
             </label>
-<<<<<<< Updated upstream
-          </template>
-      </li>
-      <li>
-        <b>Creamer: </b>
-        <template v-for="x in creamers" :key="x">
-          <label>
-              <input
-                type="radio"
-                name="creamer"
-                :id="`r${x}`"
-                :value="x"
-                v-model="currCream"
-              />
-              {{ x }}
-            </label>
-        </template>
-      </li>
-      <li>
-        <b>Syrup: </b>
-        <template v-for="x in syrups" :key="x">
-          <label>
-              <input
-                type="radio"
-                name="Syrup"
-                :id="`r${x}`"
-                :value="x"
-                v-model="currSyrup"
-              />
-              {{ x }}
-            </label>
-        </template>
-      </li>
-      <li>
-        <b>Beverage Base: </b>
-        <template v-for="x in baseBevs" :key="x">
-          <label>
-              <input
-                type="radio"
-                name="Beverage Base"
-                :id="`r${x}`"
-                :value="x"
-                v-model="currBase"
-              />
-              {{ x }}
-            </label>
-        </template>
-      </li>
-      <li>
-        <b>Save Drink: </b>
-        <input 
-          type="text"
-          name="saveTxt"
-          id="saveTxt"
-        />
-        <input 
-          type="button"
-          name="saveBtn"
-          id="saveBtn"
-          value="Save!"
-
-        />
-      </li>
-    </ul>
-=======
           </div>
         </div>
           
@@ -162,17 +86,13 @@
         value="Save!"/>
     </div>
     
->>>>>>> Stashed changes
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
 import Beverage from "./components/Beverage.vue";
-<<<<<<< Updated upstream
-=======
 import AppDropdown from "./components/AppDropdown.vue";
->>>>>>> Stashed changes
 //This is the code for the stores.
 //import { useStore } from './store.ts';
 //const store = useStore();
@@ -185,13 +105,7 @@ const currentTemp = ref("Hot");
 const currCream = ref("None");
 const currSyrup = ref("None");
 const currBase = ref("Coffee")
-<<<<<<< Updated upstream
-=======
 
-const temps = ref(["Hot", "Cold"]);
-const currentTemp = ref("Hot");
-
->>>>>>> Stashed changes
 </script>
 
 <style lang="scss">
@@ -208,8 +122,6 @@ html {
 ul {
   list-style: none;
 }
-<<<<<<< Updated upstream
-=======
 
 #selectorContainer{
   display:flex;
@@ -257,5 +169,4 @@ ul {
 }
 
 
->>>>>>> Stashed changes
 </style>
