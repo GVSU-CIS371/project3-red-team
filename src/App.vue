6c14e925a127b73e92cabcd0f363903d051d4621
ZephyrsWhisper <hardyza@mail.gvsu.edu>
2024-03-27 21:34:27 -0400

    Major Update
    Fixed and removed a bunch of code that doesn't work.

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,33 @@
   <div>
     
     <Beverage :isIced="currentTemp === 'Cold'" :creamer="currCream" :beverage="currBase" :syrup="currSyrup"/>
+<<<<<<< Updated upstream
     <ul>
       <li>
           <b>Temperature: </b>
           <template v-for="temp in temps" :key="temp">
+=======
+    
+    <div id="selectorContainer">
+
+      <AppDropdown buttonText="Temperature" class="contentButton">
+        <div class="contentBox">
+          <div v-for="temp in temps" :key="temp" class="itemBox">
+>>>>>>> Stashed changes
             <label>
               <input
                 type="radio"
                 name="temperature"
+<<<<<<< Updated upstream
                 :id="`r${temp}`"
+=======
+>>>>>>> Stashed changes
                 :value="temp"
                 v-model="currentTemp"
               />
               {{ temp }}
             </label>
+<<<<<<< Updated upstream
           </template>
       </li>
       <li>
@@ -79,12 +92,87 @@
         />
       </li>
     </ul>
+=======
+          </div>
+        </div>
+          
+      </AppDropdown>
+
+      <AppDropdown buttonText="Beverage Base" class="contentButton">
+        <div class="contentBox">
+          <div v-for="base in baseBevs" :key="base" class="itemBox">
+            <label>
+              <input
+                type="radio"
+                name="beverage_base"
+                :value="base"
+                v-model="currBase"
+              />
+              {{ base }}
+            </label>
+          </div>
+        </div>
+          
+      </AppDropdown>
+
+      <AppDropdown buttonText="Syrup" class="contentButton">
+        <div class="contentBox">
+          <div v-for="syrup in syrups" :key="syrup" class="itemBox">
+            <label>
+              <input
+                type="radio"
+                name="syrup"
+                :value="syrup"
+                v-model="currSyrup"
+              />
+              {{ syrup }}
+            </label>
+          </div>
+        </div>
+          
+      </AppDropdown>
+
+      <AppDropdown buttonText="Creamer" class="contentButton">
+        <div class="contentBox">
+          <div v-for="cream in creamers" :key="cream" class="itemBox">
+            <label>
+              <input
+                type="radio"
+                name="cream"
+                :value="cream"
+                v-model="currCream"
+              />
+              {{ cream }}
+            </label>
+          </div>
+        </div>
+      </AppDropdown>
+    </div>
+    <div id="saveButton">
+      <b>Save Drink: </b>
+      <input 
+        type="text"
+        name="saveTxt"
+        id="saveTxt"
+      />
+      <input 
+        type="button"
+        name="saveBtn"
+        id="saveBtn"
+        value="Save!"/>
+    </div>
+    
+>>>>>>> Stashed changes
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
 import Beverage from "./components/Beverage.vue";
+<<<<<<< Updated upstream
+=======
+import AppDropdown from "./components/AppDropdown.vue";
+>>>>>>> Stashed changes
 //This is the code for the stores.
 //import { useStore } from './store.ts';
 //const store = useStore();
@@ -97,6 +185,13 @@ const currentTemp = ref("Hot");
 const currCream = ref("None");
 const currSyrup = ref("None");
 const currBase = ref("Coffee")
+<<<<<<< Updated upstream
+=======
+
+const temps = ref(["Hot", "Cold"]);
+const currentTemp = ref("Hot");
+
+>>>>>>> Stashed changes
 </script>
 
 <style lang="scss">
@@ -113,4 +208,54 @@ html {
 ul {
   list-style: none;
 }
+<<<<<<< Updated upstream
+=======
+
+#selectorContainer{
+  display:flex;
+  justify-content: space-evenly;
+  background-color: white;
+  height: 20vh;
+  width: 50vw;
+}
+
+.contentButton{
+  position: relative;
+  top: 0;
+  margin: 0px 5px 0px 5px;
+  width: 25%;
+  
+}
+
+#saveButton{
+  display: flex;
+  margin: 10px 0 0 0;
+  width: 50vw;
+  background-color: whitesmoke;
+  justify-content: center;
+}
+
+.contentBox{
+  display: flex;
+  justify-content:center;
+  justify-items: center;
+  flex-direction:column;
+  margin: 0px, 2px 2px 2px;
+  background-color: tan;
+  border-radius: 3px;
+  width:100%;
+}
+
+.itemBox{
+  display: flex;
+  margin: 3px auto 3px auto;
+  padding: 2px;
+  background-color: antiquewhite;
+  border-radius: 2px;
+  width:90%;
+  text-align: left;
+}
+
+
+>>>>>>> Stashed changes
 </style>
---
+++ src/components/AppDropdown.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <slot name="toggler">
+      <button @click="toggleDropdown" class="button">{{ buttonText }}</button>
+    </slot>
+    <slot v-if="isOpen" :selected="selectedItem"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppDropdown',
+  props: {
+      buttonText: {
+          type: String,
+          required: true
+      }
+  },
+  data() {
+    return {
+      isOpen: false
+    };
+  },
+  methods: {
+    toggleDropdown() {
+      this.isOpen = !this.isOpen;
+    }
+  }
+};
+</script>
+<style>
+  .button{
+    border-radius: 5px;
+    background-color: antiquewhite;
+    width: 100%;
+  }
+
+</style>
